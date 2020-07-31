4be562183f5ded06e32f49aff87bdebe73f58afb
Geocipriano <geocipriano28@outlook.com>
2020-07-31 20:30:38 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+<v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+      color="success"
+    >
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-artist-outline</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-cog</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+      dark
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>Atual</v-toolbar-title>
+      <v-toolbar-title> Kanban</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn color="success">Login</v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+
+        </v-row>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      drawer : true,
+    }
+  }
+}
+</script>
+
+<style >
+
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav>
+        <v-toolbar dark app>
+            <v-btn round  dark @click="draw = 'true'">Tarefas</v-btn>
+            <v-spacer></v-spacer>
+            <v-toolbar-title>
+                <span class="font-italic success--text">Atual</span>
+                <span> Kanban</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn color="success">Sair</v-btn>
+        </v-toolbar>
+        <v-navigation-drawer v-model="draw" app color="success">
+            <v-btn flat block @click="drawer = false">Retornar</v-btn>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            draw : false
+        }
+    }
+}
+</script>
