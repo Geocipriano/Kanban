a524849a64b0bfe01348f5b61e934369976bb91e
Geocipriano <geocipriano28@outlook.com>
2020-08-01 01:40:16 -0300

    Configuraçao de layout
    Configuraçao de layout

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,27 +1,15 @@
 <template>
-    <nav>
-        <v-toolbar dark app>
-            <v-btn round  dark @click="draw = 'true'">Tarefas</v-btn>
-            <v-spacer></v-spacer>
-            <v-toolbar-title>
-                <span class="font-italic success--text">Atual</span>
-                <span> Kanban</span>
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn color="success">Sair</v-btn>
-        </v-toolbar>
-        <v-navigation-drawer v-model="draw" app color="success">
-            <v-btn flat block @click="drawer = false">Retornar</v-btn>
-        </v-navigation-drawer>
-    </nav>
+<v-app-bar
+  app
+  color="#304c4b"
+  >
+  <v-spacer></v-spacer>
+    <v-toolbar-title class="white--text">Kanban</v-toolbar-title>
+  <v-spacer></v-spacer>
+</v-app-bar>
 </template>
-
 <script>
 export default {
-    data() {
-        return {
-            draw : false
-        }
-    }
+    name : 'navbar',
 }
 </script>
